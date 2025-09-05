b7d5f7ad996ef181a819dab362c52425707eff3e
User <gupta.arushi2312@gmail.com>
2025-09-05 16:41:52 +0530

    Push

---
+++ src/Array/First.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class First {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        for (int x:arr){
+            System.out.println(x+" ");
+        }
+    }
+}
